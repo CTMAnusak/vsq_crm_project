a7a2e11a01f9ba2b0202d40c0d974c9c955c2506
AmpCTM <chutimon.work@gmail.com>
2025-06-17 16:34:10 +0700

    delete code of home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-// import type React from "react"
-// import type { Metadata } from "next"
-// import "./globals.css"
+import type React from "react"
+import type { Metadata } from "next"
+import "./globals.css"
 
-// export const metadata: Metadata = {
-//   title: "V Square Clinic - ลงทะเบียนสมาชิก",
-//   description: "ลงทะเบียนเป็นสมาชิก V Square Clinic เพื่อรับสิทธิพิเศษ",
-// }
+export const metadata: Metadata = {
+  title: "V Square Clinic - ลงทะเบียนสมาชิก",
+  description: "ลงทะเบียนเป็นสมาชิก V Square Clinic เพื่อรับสิทธิพิเศษ",
+}
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode
-// }>) {
-//   return (
-//     <html lang="th">
-//       <body>
-//         <div className="relative w-full max-w-768 mb-max-w-768 mx-auto overflow-x-hidden scrollbar-none">
-//           {children}
-//           </div>
-//       </body>
-//     </html>
-//   )
-// }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="th">
+      <body>
+        <div className="relative w-full max-w-768 mb-max-w-768 mx-auto overflow-x-hidden scrollbar-none">
+          {children}
+          </div>
+      </body>
+    </html>
+  )
+}
